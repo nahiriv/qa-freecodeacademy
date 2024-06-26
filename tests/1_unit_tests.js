d8f1d601f2570db7b5c454227d05ccfa842813e2
nahiriv <nahir.riveros.work@gmail.com>
2024-06-25 23:30:25 -0400

    Update 1_unit_tests.js

--- tests/1_unit_tests.js
+++ tests/1_unit_tests.js
@@ -1,1 +1,12 @@
+const chai = require('chai');
+const assert = chai.assert;
 
+suite('Unit Tests', function () {
+  suite('Basic Assertions', function () {
+    // #1
+    test('#isNull, #isNotNull', function () {
+      assert.isNull(null, 'null should be null');
+      assert.isNotNull(1, '1 is not null');
+    });
+  });
+});
